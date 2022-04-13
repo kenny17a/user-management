a9f4dc44b107cdc52e4bd647356001318fc2f81e
ibrahim kehinde <ibrahimkenny17@gmail.com>
2022-04-12 23:31:00 -0700

    user management system for school

---
+++ assets/js/index.js
@@ -0,0 +1,43 @@
+$("#add_user").submit(function(event){
+    alert("Data Inserted Successfully!")
+})
+
+$("#update_user").submit(function(event){
+    event.preventDefault();
+
+    let unindexed_array =$(this).serializeArray();
+    let data ={}
+
+    $.map(unindexed_array, function(n, i){
+        data[n["name"]] = n["value"]
+    })
+    let request = {
+        "Url":`http://127.0.0.1:3000/api/users/${data.id}`,
+        "method": "PUT",
+        "data": data
+    }
+    $.ajax(request).done(function(response){
+        alert("Data updated successfully!")
+    })
+})
+
+if (window.location.pathname == "/") {
+    $ondelete = $(".table tbody td a.delete");
+    $ondelete.click(function() {
+        let id = $(this).attr("data-id")
+
+        let request = {
+            "Url":`http://127.0.0.1:3000/api/users/${id}`,
+            "method": "DELETE",
+        
+        }
+
+        if(confirm("Do you really want to delete this record?")){
+        $.ajax(request).done(function(response){
+            alert("Data Deleted Successfully!")
+            location.reload
+        })
+    }
+
+    })
+}
